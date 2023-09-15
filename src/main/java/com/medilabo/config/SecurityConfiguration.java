0801f0167a54d455877474e9e496f3bc5bdbea2f
odeli <misterkb5@hotmail.com>
2023-09-16 00:16:20 +0200

    medilabo patient security config

--- src/main/java/com/medilabo/config/SecurityConfiguration.java
+++ src/main/java/com/medilabo/config/SecurityConfiguration.java
@@ -1,8 +1,27 @@
 package com.medilabo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfiguration {
 
+	@Bean
+	public UserDetailsService userDetailsService() {
+
+		UserDetails user = User.builder().username("user").password(passwordEncoder().encode("user123")).roles("USER")
+				.build();
+		return new InMemoryUserDetailsManager(user);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
