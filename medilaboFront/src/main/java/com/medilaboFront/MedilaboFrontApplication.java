d0fdbb1cd0e36bff7840d3cdb23bf45256457d80
odelikab <114420425+odelikab@users.noreply.github.com>
2023-09-19 17:40:31 +0200

    Add files via upload

---
+++ medilaboFront/src/main/java/com/medilaboFront/MedilaboFrontApplication.java
@@ -0,0 +1,13 @@
+package com.medilaboFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedilaboFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedilaboFrontApplication.class, args);
+	}
+
+}
---
+++ medilaboFront/src/test/java/com/medilaboFront/MedilaboFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilaboFront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedilaboFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
